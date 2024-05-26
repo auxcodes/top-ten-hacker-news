5ee95c22eb4249a4777542b7a2548495f284701e
Phill <dev@aux.codes>
2024-05-26 20:40:28 +1000

    Add noreferrer to article links

--- src/app/components/article-row.js
+++ src/app/components/article-row.js
@@ -1,7 +1,7 @@
 class ArticleRow extends HTMLElement {
   set article(article) {
     this.innerHTML = `
-              <a id='articleUrl' class='article-title' href='${article.url}' target='_blank'>${article.title}</a>
+              <a id='articleUrl' class='article-title' href='${article.url}' target='_blank' rel="noreferrer">${article.title}</a>
           `;
   }
 }
